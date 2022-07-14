f78443cb6d3c752b7bff4bd4b9b1467d8bf7f6d9
SeoEunhee <soundoftree@naver.com>
2022-07-14 16:07:53 +0900

    Add files via upload

---
+++ 춘식이 관찰일기_files/__settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1343450.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 88912554, 73603937, 71723898, 75746468, 71725255, 69812031, 75511870, 75869679, 76018110, 76063606, 76235562, 76336488, 76742599, 79678600, 79938469, 79945997, 80165527, 80165634, 80375957, 80671937, 81008160, 81140659, 81155589, 81483177, 81878622, 82692938, 83047945, 83727647, 83761025, 83992701, 85885611, 85983336 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/77937910/1/basis.wasm.js', 'wasmUrl' : 'files/assets/77937911/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/77937909/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/73586611/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/73586612/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/73586610/1/ammo.js', 'preload' : true},
+];
